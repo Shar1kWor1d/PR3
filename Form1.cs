4b0f5cbb7d28724400a40ceb14c6f301370fb189
323 <323@323-12>
2023-10-10 09:38:00 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.Entity;
+
+namespace PR3
+{
+    public partial class Form1 : Form
+    {
+        ModelEF model = new ModelEF();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartLoadData()
+        {
+            model.Users.Load();
+            rolesBindingSource.DataSource = model.Roles.ToList();
+            usersBindingSource.DataSource = model.Users.Local.ToBindingList();
+        }
+
+        private void SaveData()
+        {
+            try
+            {
+                Validate();
+                usersBindingSource.EndEdit();
+                usersBindingSource.ResetBindings(true);
+                model.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                StartLoadData();
+            }
+        }
+
+        private void usersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartLoadData();
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
